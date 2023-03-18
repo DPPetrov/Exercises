74831397733939ecae9b3e47d6b54201cee4ea01
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 22:17:01 +0200

    updates

--- SumEven.java
+++ SumEven.java
@@ -10,6 +10,16 @@ public class SumEven {
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
+        int sum = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i] % 2 == 0){
+
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
     }
 }
