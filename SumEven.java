dbf4e8df1c8c41f173655dea2c6be47f2db3cab8
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 22:10:38 +0200

    Added logic to fill array

--- SumEven.java
+++ SumEven.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class SumEven {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
 
     }
 }
