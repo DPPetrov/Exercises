4da58b46ee70e4ba905dfee5a2bfb04f300b5090
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 18:15:33 +0200

    Added days of the week, and created logic for checking which day is it

---
+++ DaysOfTheWeek.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       String[] daysArr = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+       int n = Integer.parseInt(sc.nextLine());
+
+       if(n >= 1 && n <= 7){
+           System.out.println(daysArr[n - 1]);
+       }else{
+
+       }
+    }
+}
