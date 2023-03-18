4205ac5be607584df2f230dcd83f70bd53206ba4
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 18:34:06 +0200

    Added logic for filling array

--- ReverseArray.java
+++ ReverseArray.java
@@ -7,12 +7,15 @@ public class ReverseArray {
         int n = Integer.parseInt(sc.nextLine());
         int[] array = new int[n];
 
-
-
-        for (int i = array.length - 1; i > 0 ; i--) {
+        for (int i = 0; i < array.length; i++) {
 
             int number = Integer.parseInt(sc.nextLine());
             array[i] = number;
+        }
+
+        for (int i = array.length - 1; i >= 0 ; i--) {
+
+            System.out.println(array[i] + " ");
         }
     }
 }
