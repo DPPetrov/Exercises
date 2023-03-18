a0482ae64615a2e4b05edc212f41e318166ea0d4
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 18:28:22 +0200

    Created reader input

--- ReverseArray.java
+++ ReverseArray.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class ReverseArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
     }
 }
