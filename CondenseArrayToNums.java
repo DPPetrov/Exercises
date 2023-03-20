dd4404aebe5c063397398110b00fb77227bf5032
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-20 14:35:13 +0200

    Added new loop to meet req

--- CondenseArrayToNums.java
+++ CondenseArrayToNums.java
@@ -13,5 +13,15 @@ public class CondenseArrayToNums {
 
         int[] condensedArr = new int[numsArr.length - 1];
 
+        while (numsArr.length > 1){
+
+            for (int i = 0; i < condensedArr.length; i++) {
+
+                condensedArr[i] += numsArr[i] + numsArr[i + 1];
+            }
+
+        }
+
+        System.out.println(condensedArr[0]);
     }
 }
