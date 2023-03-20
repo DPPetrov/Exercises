b54603e5d28fd7026588ef54bb607ab873e608c3
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-20 13:25:12 +0200

    Created input for both arrays

--- EqualArrays.java
+++ EqualArrays.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EqualArrays {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        int[] firstArr = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] secondArr = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
     }
 }
