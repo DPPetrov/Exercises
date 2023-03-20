c045f8dc29de7544801d806af006646649b1a416
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-20 14:08:41 +0200

    Created int[] input

--- CondenseArrayToNums.java
+++ CondenseArrayToNums.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CondenseArrayToNums {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        int[] numsArr = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
     }
 }
