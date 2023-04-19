83c514c9fbc09dfe2e29ef2ca5a63be81804e2a7
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-04-19 18:52:59 +0300

    created getters and setters

--- Car.java
+++ Car.java
@@ -3,4 +3,28 @@ public class Car {
     private String model;
     private String brand;
     private int horsePower;
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
 }
