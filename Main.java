e439e61b69404b8d61dc6757b3913a3fd4e8d90b
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-04-19 18:51:24 +0300

    Created new car Object

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car car = new Car();
+
+        car.brand = "Chevrolet";
+        car.model = "Impala";
+        car.horsePower = 390;
+
+        System.out.println(String.format("The car is: %s %s - %d HP", car.brand, car.model, car.horsePower));
     }
 }
