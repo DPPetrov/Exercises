4311026329ceaac2a92aa05797709b261eb51970
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-04-19 18:55:24 +0300

    modified code with getters and setters

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ public class Main {
 
         Car car = new Car();
 
-        car.brand = "Chevrolet";
-        car.model = "Impala";
-        car.horsePower = 390;
+        car.setBrand("Chevrolet");
+        car.setBrand("Impala");
+        car.setHorsePower(390);
 
-        System.out.println(String.format("The car is: %s %s - %d HP", car.brand, car.model, car.horsePower));
+        System.out.println(String.format("The car is: %s %s - %d HP", car.getBrand(), car.getModel(), car.getHorsePower()));
     }
 }
