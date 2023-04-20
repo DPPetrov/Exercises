c9b1cdac460a027f844156ba45d64088cb5acaf4
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-04-20 08:45:37 +0300

    Added scanner and loop

--- CarConstructors/Main.java
+++ CarConstructors/Main.java
@@ -1,9 +1,17 @@
 package CarConstructors;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
 
+        while (n > 0){
+
+            n--;
+        }
 
     }
 }
